b4fb36de2fd586ca391159493cac08b1a84e650c
Tomohiro Kawakami <acorn310@gmail.com>
2019-11-29 01:13:24 +0900

    plot用のスクリプトを追加

---
+++ util/plot_data.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    data = np.genfromtxt('../output/agent0_log.txt',dtype=None, names=True)
+    x_label = 'Samples'
+    y_label = list(data.dtype.names[3:5])
+
+    plot_graph(data[x_label], np.array(data[y_label].tolist()), x_label, y_label, x_lim = None, y_lim = None, legend = y_label, f_name = None, times_new_roman = False)
+
+def plot_graph(x, y, x_label, y_label, x_lim = None, y_lim = None, legend = None, f_name = None, times_new_roman = False, rc_params = None):
+    if rc_params:
+        if times_new_roman:
+            import matplotlib.font_manager as fon
+            del fon.weight_dict['roman']
+            fon._rebuild()
+            plt.rcParams['font.family'] = 'Times New Roman' # font
+        else:
+            plt.rcParams['font.family'] = 'sans-serif'
+        plt.rcParams['mathtext.default'] = 'regular' # 数式フォント
+        plt.rcParams['xtick.top'] = 'True'
+        plt.rcParams['ytick.right'] = 'True'
+        plt.rcParams['xtick.direction'] = 'in'
+        plt.rcParams['ytick.direction'] = 'in'
+        plt.rcParams['xtick.major.width'] = 1.0 #x軸主目盛り線の線幅
+        plt.rcParams['ytick.major.width'] = 1.0 #y軸主目盛り線の線幅
+        plt.rcParams['axes.grid'] = 'True'
+        plt.rcParams['axes.xmargin'] = '0' #'.05'
+        plt.rcParams['axes.ymargin'] = '.05'
+        plt.rcParams['savefig.facecolor'] = 'None'
+        plt.rcParams['savefig.edgecolor'] = 'None'
+        plt.rcParams['savefig.bbox'] = 'tight'
+        plt.rcParams['font.size'] = 14 #フォントの大きさ
+        plt.rcParams["legend.fancybox"] = False # 丸角
+        plt.rcParams["legend.framealpha"] = 1 # 透明度の指定、0で塗りつぶしなし
+        plt.rcParams["legend.edgecolor"] = 'black' # edgeの色を変更
+        plt.rcParams["legend.handlelength"] = 1 # 凡例の線の長さを調節
+        # plt.rcParams["legend.labelspacing"] = 5. # 垂直方向（縦）の距離の各凡例の距離
+        # plt.rcParams["legend.handletextpad"] = 3. # 凡例の線と文字の距離の長さ
+        # plt.rcParams["legend.markerscale"] = 2 # 点がある場合のmarker scale
+
+    fig = plt.figure()
+    fig_1 = fig.add_subplot(111)
+    y_shape = y.shape
+    for yn in range(y_shape[1]):
+        if legend != None:
+            fig_1.plot(x, y[:, yn], label = legend[yn])
+        else:
+            fig_1.plot(x, y[:, yn])
+    fig_1.set_xlabel(x_label)
+    fig_1.set_ylabel(y_label)
+    if x_lim != None:
+        fig_1.set_xlim(x_lim[0], x_lim[1])
+    if y_lim != None:
+        fig_1.set_ylim(y_lim[0], y_lim[1])
+    if legend != None:
+        fig_1.legend()
+    if f_name != None:
+        plt.savefig(f_name, bbox_inches="tight", pad_inches=0.05)
+    else:
+        plt.show()
+
+if __name__ == '__main__':
+    main()
