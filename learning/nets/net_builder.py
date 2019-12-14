282b1d9a016665859fd9bab21bee4e961d8f3b3b
Tomohiro Kawakami <acorn310@gmail.com>
2019-12-15 00:11:01 +0900

    中間層を256, 128に変更した

--- learning/nets/net_builder.py
+++ learning/nets/net_builder.py
@@ -1,10 +1,10 @@
-import learning.nets.fc_3layers_1024units as fc_3layers_1024units
+import learning.nets.fc_2layers_256units as fc_2layers_256units
 
 def build_net(net_name, input_tfs, reuse=False):
     net = None
 
-    if (net_name == fc_3layers_1024units.NAME):
-        net = fc_3layers_1024units.build_net(input_tfs, reuse)
+    if (net_name == fc_2layers_256units.NAME):
+        net = fc_2layers_256units.build_net(input_tfs, reuse)
     else:
         assert False, 'Unsupported net: ' + net_name
     
