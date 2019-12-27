f6f7b62cfb0e6b8c6957274b08d972d2ab632ab0
Tomohiro Kawakami <acorn310@gmail.com>
2019-12-27 23:37:25 +0900

    reward関数からrootとcogを削除した

--- DeepMimicCore/scenes/SceneImitate.cpp
+++ DeepMimicCore/scenes/SceneImitate.cpp
@@ -22,11 +22,19 @@ double cSceneImitate::CalcRewardImitate(const cSimCharacter& sim_char, const cKi
 	// double root_w = 0.67;
 	// double com_w = 0.33;
 
+	// without root
+	// double pose_w = 0.5;
+	// double vel_w = 0.05;
+	// double end_eff_w = 0.15;
+	// double root_w = 0.0;
+	// double com_w = 0.1;
+
+	// without root and com
 	double pose_w = 0.5;
 	double vel_w = 0.05;
 	double end_eff_w = 0.15;
 	double root_w = 0.0;
-	double com_w = 0.1;
+	double com_w = 0.0;
 
 	double total_w = pose_w + vel_w + end_eff_w + root_w + com_w;
 	pose_w /= total_w;
